f764b194a0019edea146683c715cd2c8e8e28ad8
kotamori9 <kotamori27@gmail.com>
2020-05-06 08:52:50 +0900

    リスト2−8

--- routes/web.php
+++ routes/web.php
@@ -17,25 +17,4 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('hello/{msg?}', function ($msg='no message.') {
-
-$html = <<<EoF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size:16px; color:#999; }
-h1 { font-size:100pt; text-align:right; color:#eee;
-    margin:-40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <p>{$msg}</p>
-    <p>これはサンプルページです。</p>
-</body>
-</html>
-EoF;
-
-  return $html;
-});
+Route::get('hello', 'HelloController@index');
