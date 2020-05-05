e0a3f416d9ea9a93d95e3d664a8d87143417da8c
kotamori9 <kotamori27@gmail.com>
2020-05-05 18:08:32 +0900

    リスト2−5

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-$html = <<<EoF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size:16px; color:#999; }
-h1 { font-size:100pt; text-align:right; color:#eee;
-    margin:-40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <p>This is sample page.</p>
-    <p>これはサンプルページです。</p>
-</body>
-</html>
-EoF;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -35,6 +17,25 @@ EoF;
 //     return view('welcome');
 // });
 
-Route::get('hello', function () use ($html) {
-    return $html;
+Route::get('hello/{msg}', function ($msg) {
+
+$html = <<<EoF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size:16px; color:#999; }
+h1 { font-size:100pt; text-align:right; color:#eee;
+    margin:-40px 0px -50px 0px; }
+</style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>{$msg}</p>
+    <p>これはサンプルページです。</p>
+</body>
+</html>
+EoF;
+
+  return $html;
 });
