89810922b64e72d7bb5ca99d22e97e25951921a6
kotamori9 <kotamori27@gmail.com>
2020-05-06 20:52:24 +0900

    リスト２−１５

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function __invoke(){
-        return <<<EOF
+    public function index(Request $request,Response $response){
+return <<<EOF
 <html>
 <head>
-<title>Hello</title>
+<title>Hello/Index</title>
 <style>
 body {font-size:16pt; color:#999; }
 h1 { font-size:30pt; text-align:right; color:#eee;
@@ -18,11 +19,15 @@ h1 { font-size:30pt; text-align:right; color:#eee;
 </style>
 </head>
 <body>
-    <h1>Single action</h1>
-    <p>これは、シングルアクションコントローラーのアクションです。</p>
+    <h1>Hello</h1>
+    <h3>Request</h3>
+    <pre>{$request}</pre>
+    <h3>Response</h3>
+    <pre>{$response}</pre>
 </body>
 </html>
 EOF;
-
+        $response->setContent($html);
+        return $response;
     }
 }
