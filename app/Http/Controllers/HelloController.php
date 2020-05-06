5ed6073e31ad21adda2d3273d5ce9c7e2dc12069
kotamori9 <kotamori27@gmail.com>
2020-05-06 13:03:56 +0900

    リスト２−１４

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,21 +8,21 @@ class HelloController extends Controller
 {
     public function __invoke(){
         return <<<EOF
-    <html>
-    <head>
-    <title>Hello</title>
-    <style>
-    body {font-size:16pt; color:#999; }
-    h1 { font-size:30pt; text-align:right; color:#eee;
-        margin:-15px 0px 0px 0px; }
-    </style>
-    </head>
-    <body>
-        <h1>Single action</h1>
-        <p>これは、シングルアクションコントローラーのアクションです。</p>
-    </body>
-    </html>
-    EOF;
-    
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size:16pt; color:#999; }
+h1 { font-size:30pt; text-align:right; color:#eee;
+    margin:-15px 0px 0px 0px; }
+</style>
+</head>
+<body>
+    <h1>Single action</h1>
+    <p>これは、シングルアクションコントローラーのアクションです。</p>
+</body>
+</html>
+EOF;
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,4 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('hello', 'HelloController@index');
-Route::get('hello/other', 'HelloController@other');
+Route::get('hello', 'HelloController');
