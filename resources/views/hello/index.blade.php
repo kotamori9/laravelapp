5a3ae8c22a325df341122f40dda0b7f07a428dae
kotamori9 <kotamori27@gmail.com>
2020-05-08 17:29:16 +0900

    リスト3−22

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,16 +8,12 @@
   </style>
 </head>
 <body>
-    <h1>Blade/Index</h1>
-    @isset ($msg)
-    <p>こんにちは、{{$msg}}さん。</p>
-    @else
-    <p>何か書いてください。</p>
-    @endisset
-    <form method="POST" action="/hello">
-      @csrf
-      <input type="text" name="msg">
-      <input type="submit">
-    </form>
+  <h1>Blade/Index</h1>
+  <p>&#064;foreachディレクティブの例</p>
+  <ol>
+  @foreach($data as $item)
+  <li>{{$item}}
+  @endforeach
+  </ol>
 </body>
 </html>
