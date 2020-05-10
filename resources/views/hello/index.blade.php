2ba6d5cdbb8e476cba589402f06c7a40b0c2b2a7
kotamori9 <kotamori27@gmail.com>
2020-05-10 18:13:51 +0900

    リスト3−37

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -7,8 +7,7 @@
 
 @section('content')
   <p>ここが本文のコンテンツです。</p>
-  <ul>
-    @each('components.item',$data,'item')
-  </ul>
+  <p>Controller value<br>'message' = {{$message}}</p>
+  <p>ViewComposer value<br>'view_message' = {{$view_message}}</p>
 
 @endsection
