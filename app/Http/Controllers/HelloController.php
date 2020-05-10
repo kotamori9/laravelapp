b65a8b605ca62987f49b5f93ae0ed4c946a98aaf
kotamori9 <kotamori27@gmail.com>
2020-05-11 01:13:59 +0900

    リスト4−5

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request　$request)
+    public function index(Request $request)
     {
         return view('hello.index',['data'=>$request->data]);
     }
