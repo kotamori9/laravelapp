3de8666dfcc926a67086b4c965a2e03e91929ef7
kotamori9 <kotamori27@gmail.com>
2020-05-10 18:22:21 +0900

    リスト3−38

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,12 +9,7 @@ class HelloController extends Controller
 {
     public function index()
     {
-        $data = [
-            ['name'=>'山田太郎','mail'=>'taro@yamada'],
-            ['name'=>'田中花子','mail'=>'hanako@flower'],
-            ['name'=>'鈴木幸子','mail'=>'sachiko@happy']
-        ];
-        return view('hello.index', ['data'=>$data]);
+        return view('hello.index',['message'=>'Hello!']);
     }
 
     public function post(Request $request)
