4f29c9e4c09b3a796e0e9e89fb8e1db5f7d184cc
kotamori9 <kotamori27@gmail.com>
2020-05-21 18:14:55 +0900

    リスト5−5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,6 +61,11 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            th{background-color:#999; color:fff; padding:5px 10px; }
+
+            td{border: solid 1px #aaa; color:#999; padding:5px 10px; }
+
         </style>
     </head>
     <body>
